08d85a29fe6c181447781bce881986c3e7c7690d
loveKino <842914439@qq.com>
2019-03-24 23:45:38 +0800

    support quick start tcp service

--- pcpService.go
+++ pcpService.go
@@ -6,9 +6,12 @@ package gopcp_service
 //  (3) sandbox provided functions
 
 import (
+	"github.com/idata-shopee/gopcp"
+	rpc "github.com/idata-shopee/gopcp_rpc"
 	"log"
 	"net/http"
 	"strconv"
+	"sync"
 )
 
 type Attachment struct {
@@ -24,6 +27,20 @@ type HttpConn struct {
 
 func StartHttpServer(port int, routes []Route) error {
 	router := GetRouter(routes)
-	log.Println("try to start server at " + strconv.Itoa(port))
+	log.Println("try to start http server at " + strconv.Itoa(port))
 	return http.ListenAndServe(":"+strconv.Itoa(port), router)
+}
+
+func StartTcpServer(port int, sandbox *gopcp.Sandbox) error {
+	log.Println("try to start tcp server at " + strconv.Itoa(port))
+	if server, err := rpc.GetPCPRPCServer(port, sandbox); err != nil {
+		return err
+	} else {
+		defer server.Close()
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+		wg.Wait()
+		return nil
+	}
 }
