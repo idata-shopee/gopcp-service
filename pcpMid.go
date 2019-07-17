9faed4295336245b01360cba8920b9fd4105aad1
loveKino <842914439@qq.com>
2019-07-17 10:58:59 +0800

    add klog

--- pcpMid.go
+++ pcpMid.go
@@ -3,10 +3,14 @@ package gopcp_service
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	KLog "github.com/lock-free/goklog"
 	"github.com/lock-free/gopcp"
 	"net/http"
 	"net/url"
 )
+
+var klog = KLog.GetInstance()
 
 type PcpHttpResponse struct {
 	Data   interface{} `json:"text"`
@@ -49,6 +53,15 @@ func GetPcpMid(sandbox *gopcp.Sandbox) MidFunType {
 		var rawQuery string
 		var ret interface{}
 
+		// recover if panic
+		defer func() {
+			if r := recover(); r != nil {
+				panicErr := fmt.Errorf("panic happened at pcp mid, return is %v", r)
+				klog.LogError("pcp-mid-exception", panicErr)
+				w.Write(ResponseToBytes(ErrorToResponse(err, 530)))
+			}
+		}()
+
 		if r.Method == "GET" {
 			rawQuery, err = url.QueryUnescape(r.URL.RawQuery)
 			if err == nil {
@@ -73,6 +86,11 @@ func GetPcpMid(sandbox *gopcp.Sandbox) MidFunType {
 		}
 
 		w.Write(ResponseToBytes(pcpHttpRes))
+
+		if err != nil {
+			klog.LogError("pcp-mid-exception", err)
+		}
+
 		return arr, err
 	}
 }
